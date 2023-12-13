2bda4fc240772ef96059c521dad14c344b8b7b8a
unknown <630836058@qq.com>
2023-12-13 22:12:56 +0800

    feat: vir-list

---
+++ src/pages/algorithm-complexity/components/List.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+
+export default function List({ children, containerHeight, data, itemHeight }) {
+  const Children = children;
+  // console.log(Children);
+
+  const contentHeight = data.length * itemHeight;
+  const [top, setTop] = useState(0);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const [renderItems, setRenderItems] = useState([]);
+  useEffect(() => {
+    console.log(scrollTop);
+
+    const startIndex = scrollTop / itemHeight;
+    const endIndex = (scrollTop + containerHeight) / itemHeight;
+
+    const top = itemHeight * startIndex;
+    setTop(top);
+    console.log(startIndex, endIndex, top);
+
+    // const items = data.slice(startIndex, endIndex + 1);
+    // setRenderItems(items);
+  }, [scrollTop]);
+
+  const handleScroll = (e) => {
+    setScrollTop(e.target.scrollTop);
+  };
+  return (
+    <div
+      className="my-virtual-list"
+      style={{
+        height: containerHeight,
+        overflow: 'auto',
+      }}
+      onScroll={handleScroll}
+    >
+      <div
+        style={{
+          height: contentHeight,
+        }}
+      >
+        <div style={{ height: top }}></div>
+        <div>
+          {renderItems.map((item, idx) => {
+            return <div key={idx}>{item}</div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
